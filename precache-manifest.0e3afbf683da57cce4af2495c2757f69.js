cb9403e85c389d5ab04ed4d72b86d8282fe6c6e8
Antoni Petrenko <flay9123@gmail.com>
2020-02-08 14:12:54 +0200

    Updates

---
+++ precache-manifest.0e3afbf683da57cce4af2495c2757f69.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038afa63231017acf66ac0dae6a27030",
+    "url": "/Restorant/index.html"
+  },
+  {
+    "revision": "c096c202860e8af7cfb5",
+    "url": "/Restorant/static/css/main.a6ffccfc.chunk.css"
+  },
+  {
+    "revision": "aec88f81ca6758d41667",
+    "url": "/Restorant/static/js/2.0d2e2ebb.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/Restorant/static/js/2.0d2e2ebb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c096c202860e8af7cfb5",
+    "url": "/Restorant/static/js/main.1457901f.chunk.js"
+  },
+  {
+    "revision": "db54e470b61c5ac57afb",
+    "url": "/Restorant/static/js/runtime-main.ed771122.js"
+  },
+  {
+    "revision": "811394cd0dfda2b382d1bd263ef10db1",
+    "url": "/Restorant/static/media/header1278.811394cd.jpg"
+  },
+  {
+    "revision": "a591aa2ae77c748dd3479cdb5547da38",
+    "url": "/Restorant/static/media/local2.a591aa2a.jpg"
+  },
+  {
+    "revision": "be5850f76b12f8d54eacc16e76faaef4",
+    "url": "/Restorant/static/media/loftSmall.be5850f7.png"
+  },
+  {
+    "revision": "a60016aadf0b0fd939c0e815bd05e018",
+    "url": "/Restorant/static/media/polska.a60016aa.png"
+  },
+  {
+    "revision": "a5a81923fc0bca6bc853c8056a9141f7",
+    "url": "/Restorant/static/media/spinachi.a5a81923.png"
+  },
+  {
+    "revision": "fd97525e87668b45e587f7355f665d01",
+    "url": "/Restorant/static/media/telephone.fd97525e.jpg"
+  },
+  {
+    "revision": "4154c06e83bf9a8f1598212155d24473",
+    "url": "/Restorant/static/media/zielonomi.4154c06e.png"
+  }
+]);
